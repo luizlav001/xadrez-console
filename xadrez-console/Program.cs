716aaf2de223dc4864891cdc26c0507f0ec133e9
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-20 12:29:16 -0300

    Adicionar arquivos de projeto.

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+        }
+        
+    }
+}
